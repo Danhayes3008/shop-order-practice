29ba9abd1b3d88f6cb1832f27385a78245a1a041
Danhayes3008 <43347669+Danhayes3008@users.noreply.github.com>
2019-06-11 13:46:13 +0100

    Update min-order-form.py
    added a product name and price. I also included a print stating how much the sale costs.

--- min-order-form.py
+++ min-order-form.py
@@ -1,20 +1,25 @@
 """
 Input an amount to have the amount bought removed from the stock level
 """
-
-sale = input(" ")
+productName = "Golf balls"
+price = 1.99
+sale = input("How many would you like to buy: ")
 maxStock = 2500
 minStock = 2000
 stockLevel = 2250
 stockAvailable = stockLevel - int(sale)
 stock = int(stockAvailable)
 order = minStock - int(stock)
+totalCost = price * int(sale)
 
-print('Sale: ', sale)
+print(productName)
+print('Cost:', "£",price)
+print('Sale:', sale)
 print('Max stock: ', maxStock)
 print('Min stock: ', minStock)
 print('Stock level: ', stockLevel)
 print('Stock available: ', stockAvailable)
+print("£", totalCost)
 
 if int(stock) > minStock:
     print("no stock is needed")
